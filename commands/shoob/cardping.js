e87107002fa84ca31ece1cb9bc87e5e92706c559
Nugroho <74390128+Nugroho20@users.noreply.github.com>
2021-01-13 14:51:36 +0700

    Update cardping.js

--- commands/shoob/cardping.js
+++ commands/shoob/cardping.js
@@ -8,13 +8,13 @@ exports.run = async (client, message, args) => {
         return message.channel.send('Please activate first to use Shoob Category Commands');
     }
     if(!message.member.permissions.any(["MANAGE_GUILD", "ADMINISTRATOR"])) {
-        return message.channel.send('<:Mayuri_Failed:772486728300101632>| You need **Manage Server** permission to perform this command!');
+        return message.channel.send(':red_circle:| You need **Manage Server** permission to perform this command!');
     }
 
     let tier = ["t4", "T4", "t5", "T5", "t6", "T6"];
     if (!tier.includes(args[0])) {
         return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Wrong Usage m!setping `T4/T5/T6 <@role>`",
+            "description": ":red_circle:| Wrong Usage m!setping `T4/T5/T6 <@role>`",
             "color": "RED"
         }});
     }
@@ -22,85 +22,85 @@ exports.run = async (client, message, args) => {
     if (args[0] === "t4") {
         let roles = message.mentions.roles.first()
         if (!roles) return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Please Provide the role that you want to make it as `card-mention`",
+            "description": ":red_circle:| Please Provide the role that you want to make it as `card-mention`",
             "color": "RED"
         }});
 
         await client.db.set(`t4ping_${message.guild.id}`, `<@&${roles.id}>`)
         await client.db.set(`t4ping.${message.guild.id}`, `on`)
         message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Ping for `T4` card is seted on role <@&" + roles.id + ">",
-            "color": "GREEN"
+            "description": ":white_circle:| Ping for `T4` card is seted on role <@&" + roles.id + ">",
+            "color": "WHITE"
         }});
     }
     if (args[0] === "T4") {
         let roles = message.mentions.roles.first()
         if (!roles) return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Please Provide the role that you want to make it as `card-mention`",
+            "description": ":red_circle:| Please Provide the role that you want to make it as `card-mention`",
             "color": "RED"
         }});
 
         await client.db.set(`t4ping_${message.guild.id}`, `<@&${roles.id}>`)
         await client.db.set(`t4ping.${message.guild.id}`, `on`)
         message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Ping for `Tier 4` card is seted on role <@&" + roles.id + ">",
-            "color": "GREEN"
+            "description": ":white_circle:| Ping for `Tier 4` card is seted on role <@&" + roles.id + ">",
+            "color": "WHITE"
         }});
     }
     if (args[0] === "t5") {
         let roles = message.mentions.roles.first()
         if (!roles) return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Please Provide the role that you want to make it as `card-mention`",
+            "description": ":red_circle:| Please Provide the role that you want to make it as `card-mention`",
             "color": "RED"
         }});
 
         await client.db.set(`t5ping_${message.guild.id}`, `<@&${roles.id}>`)
         await client.db.set(`t5ping.${message.guild.id}`, `on`)
         message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Ping for `Tier 5` card is seted on role <@&" + roles.id + ">",
-            "color": "GREEN"
+            "description": ":white_circle:| Ping for `Tier 5` card is seted on role <@&" + roles.id + ">",
+            "color": "WHITE"
         }});
     }
     if (args[0] === "T5") {
         let roles = message.mentions.roles.first()
         if (!roles) return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Please Provide the role that you want to make it as `card-mention`",
+            "description": ":red_circle:| Please Provide the role that you want to make it as `card-mention`",
             "color": "RED"
         }});
 
         await client.db.set(`t5ping_${message.guild.id}`, `<@&${roles.id}>`)
         await client.db.set(`t5ping.${message.guild.id}`, `on`)
         message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Ping for `Tier 5` card is seted on role <@&" + roles.id + ">",
-            "color": "GREEN"
+            "description": ":white_circle:| Ping for `Tier 5` card is seted on role <@&" + roles.id + ">",
+            "color": "WHITE"
         }});
     }
     if (args[0] === "t6") {
         let roles = message.mentions.roles.first()
         if (!roles) return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Please Provide the role that you want to make it as `card-mention`",
+            "description": ":red_circle:| Please Provide the role that you want to make it as `card-mention`",
             "color": "RED"
         }});
 
         await client.db.set(`t6ping_${message.guild.id}`, `<@&${roles.id}>`)
         await client.db.set(`t6ping.${message.guild.id}`, `on`)
         message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Ping for `Tier 6` card is seted on role <@&" + roles.id + ">",
-            "color": "GREEN"
+            "description": ":white_circle:| Ping for `Tier 6` card is seted on role <@&" + roles.id + ">",
+            "color": "WHITE"
         }});
     }
     if (args[0] === "T6") {
         let roles = message.mentions.roles.first()
         if (!roles) return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Please Provide the role that you want to make it as `card-mention`",
+            "description": ":red_circle:| Please Provide the role that you want to make it as `card-mention`",
             "color": "RED"
         }});
 
         await client.db.set(`t6ping_${message.guild.id}`, `<@&${roles.id}>`)
         await client.db.set(`t6ping.${message.guild.id}`, `on`)
         message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Ping for `Tier 6` card is seted on role <@&" + roles.id + ">",
-            "color": "GREEN"
+            "description": ":white_circle:| Ping for `Tier 6` card is seted on role <@&" + roles.id + ">",
+            "color": "WHITE"
         }});
     }
 }
