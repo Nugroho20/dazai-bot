7723d0f900b54ba2d623b93d9fb89c55653f8bfc
Nugroho <74390128+Nugroho20@users.noreply.github.com>
2021-01-13 17:36:31 +0700

    Update cardlog.js

--- commands/shoob/cardlog.js
+++ commands/shoob/cardlog.js
@@ -7,13 +7,13 @@ exports.run = async (client, message, args) => {
         return message.channel.send('Please activate first to use Shoob Category Commands');
     }
     if(!message.member.permissions.any(["MANAGE_GUILD", "ADMINISTRATOR"])) {
-        return message.channel.send('<:Mayuri_Failed:772486728300101632>| You need **Manage Server** permission to perform this command!');
+        return message.channel.send(':red_circle: | You need **Manage Server** permission to perform this command!');
     }
 
     let toggling = ["disable", "enable", "false", "true"];
     if (!toggling.includes(args[0])) {
         return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Wrong Usage m!cardlog `true/false`",
+            "description": ":red_circle: | Wrong Usage Nez!cardlog `true/false`",
             "color": "RED"
         }});
     }
@@ -21,55 +21,55 @@ exports.run = async (client, message, args) => {
     if (args[0] === "true") {
         let channel = message.mentions.channels.first()
         if (!channel) return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Please provide the channel that you want to make it as `card-log`",
+            "description": ":red_circle:| Please provide the channel that you want to make it as `card-log`",
             "color": "RED"
         }});
 
         await client.db.set(`channel_${message.guild.id}`, channel.id)
         await client.db.set(`shooblog_${message.guild.id}`, `true`)
         message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Card Log now will be displayed in <#" + channel.id + ">",
-            "color": "GREEN"
+            "description": ":white_circle:| Card Log now will be displayed in <#" + channel.id + ">",
+            "color": "WHITE"
         }});
     }
     if (args[0] === "enable") {
         let channel = message.mentions.channels.first()
         if (!channel) return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Please provide the channel that you want to make it as `card-log`",
+            "description": ":white_circle:| Please provide the channel that you want to make it as `card-log`",
             "color": "RED"
         }});
 
         await client.db.set(`channel_${message.guild.id}`, channel.id)
         await client.db.set(`shooblog_${message.guild.id}`, `true`)
         message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Card Log now will be displayed in <#" + channel.id + ">",
-            "color": "GREEN"
+            "description": ":white_circle:| Card Log now will be displayed in <#" + channel.id + ">",
+            "color": "WHITE"
         }});
     }
     if (args[0] === "false") {
         let toggle = await client.db.get(`shooblog_${message.guild.id}`)
         if (!toggle || toggle === "null") return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Card-log is already `disabled` in this server",
+            "description": ":red_circle:| Card-log is already `disabled` in this server",
             "color": "RED"
         }});
         await client.db.delete(`shooblog_${message.guild.id}`)
         await client.db.delete(`channel_${message.guild.id}`)
         return  message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Card-log has been disabled",
-            "color": "GREEN"
+            "description": ":white_circle:| Card-log has been disabled",
+            "color": "WHITE"
         }});
     }
     if (args[0] === "disable") {
         let toggle = await client.db.get(`shooblog_${message.guild.id}`)
         if (!toggle || toggle === "null") return message.channel.send({ embed: {
-            "description": "<:Mayuri_Failed:772486728300101632>| Card-log is already `disabled` in this server",
+            "description": ":red_circle:| Card-log is already `disabled` in this server",
             "color": "RED"
         }});
         await client.db.delete(`shooblog_${message.guild.id}`)
         await client.db.delete(`channel_${message.guild.id}`)
         return  message.channel.send({ embed: {
-            "description": "<:Mayuri_Success:772486748864249857>| Card-log has been disabled",
-            "color": "GREEN"
+            "description": ":white_circle:| Card-log has been disabled",
+            "color": "WHITE"
         }});
     }
 
