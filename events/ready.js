0cac3c7479409b3b8717cd1344f660273a5ef98a
Nugroho <74390128+Nugroho20@users.noreply.github.com>
2021-01-14 19:03:34 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    client.user.setActivity(`MAINTENANCE NOW XD`, {
+    client.user.setActivity(`Hey, do you want to kill yourself with me? XD`, {
     type: "PLAYING"
   })
   console.log(`${client.user.username} Is now Online !`)
